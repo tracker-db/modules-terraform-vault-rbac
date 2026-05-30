9e1aa261c1da66dcc637f33f78f8e6c6e0f0a556
Erich (EJ) Best <20603184+ejbest@users.noreply.github.com>
2026-05-30 13:52:00 -0400

    feat: accept vault_admin and secret_paths in role definitions

--- variables.tf
+++ variables.tf
@@ -1,10 +1,14 @@
 # modules/vault-rbac/variables.tf
+# Updated to accept the full role structure from v3
 
 variable "roles" {
   description = "Role definitions from roles.tf"
   type = map(object({
-    description = string
+    description  = string
+    vault_admin  = optional(bool, false)
+    secret_paths = optional(list(string), [])
     grants = map(object({
+      type    = optional(string, "ssh")
       targets = list(string)
       access  = string
       ttl     = string
